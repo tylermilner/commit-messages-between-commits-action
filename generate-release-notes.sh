6084f1f1e84b756c5c98a4f672ad59cf2075d26e
Tyler Milner <tylermilner@gmail.com>
2024-04-20 02:14:01 -0500

    Fix remaining linter errors.

--- generate-release-notes.sh
+++ generate-release-notes.sh
@@ -4,17 +4,17 @@
 
 # Validate environment variable inputs
 if [[ -z "$INPUT_END_SHA" ]]; then
-    # In theory, this should never happen since `action.yml` sets the default value of `end-sha` to the current commit
-    echo "Error: Unable to generate release notes. Missing 'end-sha' input."
-    exit 1
+	# In theory, this should never happen since `action.yml` sets the default value of `end-sha` to the current commit
+	echo "Error: Unable to generate release notes. Missing 'end-sha' input."
+	exit 1
 fi
 
 # Determine the SHA to start from
 INITIAL_SHA="$INPUT_BEGIN_SHA"
 
 if [[ -z "$INPUT_BEGIN_SHA" ]]; then
-    echo "Missing 'begin-sha' input. Defaulting to initial commit."
-    INITIAL_SHA=$(git rev-list --max-parents=0 HEAD)
+	echo "Missing 'begin-sha' input. Defaulting to initial commit."
+	INITIAL_SHA=$(git rev-list --max-parents=0 HEAD)
 fi
 
 # Get the commit history between begin and end SHAs
@@ -32,16 +32,16 @@ echo "$RELEASE_NOTES"
 
 # Check if a filename is provided as input
 if [[ -n "$RELEASE_NOTES_FILE" ]]; then
-    # Save the release notes to the specified file
-    echo "$RELEASE_NOTES" > "$RELEASE_NOTES_FILE"
-    echo "Saved release notes to $RELEASE_NOTES_FILE"
+	# Save the release notes to the specified file
+	echo "$RELEASE_NOTES" >"$RELEASE_NOTES_FILE"
+	echo "Saved release notes to $RELEASE_NOTES_FILE"
 fi
 
 # Output multiline string. See https://github.com/orgs/community/discussions/26288#discussioncomment-3876281
 echo "Saving to GitHub output..."
 delimiter="$(openssl rand -hex 8)"
 {
-    echo "release-notes<<${delimiter}"
-    echo "$RELEASE_NOTES"
-    echo "${delimiter}"
+	echo "release-notes<<${delimiter}"
+	echo "$RELEASE_NOTES"
+	echo "${delimiter}"
 } >> "${GITHUB_OUTPUT}"
